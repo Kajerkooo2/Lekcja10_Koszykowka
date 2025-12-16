36f650eaac5bd6841e35a72eb00240e641b98caa
Kajerkooo2 <prokajerko79@gmail.com>
2025-12-16 08:09:59 +0100

    binding podstawy

--- app/src/main/java/com/example/lekcja10_koszykowka/MainActivity.java
+++ app/src/main/java/com/example/lekcja10_koszykowka/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.lekcja10_koszykowka;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,8 +9,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.lekcja10_koszykowka.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding; //klasa z bindowanymi widokami
+    //dla pliku acitivtymain.xml jest ActivityMainBinding
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +25,10 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+        binding.textView.setText("0");
     }
 }
